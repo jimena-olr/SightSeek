70a779d35c268150a0c559dd134f851f8eddeb8c
Jimena Olivares R. <jimena.olivares.rivera28@gmail.com>
2024-10-04 20:25:23 -0400

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'damm.rodrigo.jimena.mario',
+  appName: 'Sightseek',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
